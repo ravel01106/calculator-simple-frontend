1d67e7a77017da218f576ca9d687b01684ed24c2
ravel01106 <ravello2002@gmail.com>
2024-06-04 12:42:41 +0100

    feat(App.tsx): visually created calculator.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import React from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main-container'>
+      <div className='calculator-container'>
+        <div className='screen'></div>
+        <table className='container-buttons'>
+          <tr className="buttons">
+            <td><button>C</button></td>
+            <td><button>/</button></td>
+            <td><button>*</button></td>
+            <td><button>-</button></td>
+          </tr>
+          <tr className="buttons">
+            <td><button>7</button></td>
+            <td><button>8</button></td>
+            <td><button>9</button></td>
+            <td rowSpan={2} className='button-row'><button>+</button></td>
+          </tr>
+          <tr className="buttons">
+            <td><button>6</button></td>
+            <td><button>5</button></td>
+            <td><button>4</button></td>
+          </tr>
+          <tr className="buttons">
+            <td><button>3</button></td>
+            <td><button>2</button></td>
+            <td><button>1</button></td>
+            <td rowSpan={2} className='button-row'><button>=</button></td>
+          </tr>
+          <tr className="buttons">
+            <td colSpan={2} className='button-col'><button>0</button></td>
+            <td><button>.</button></td>
+            
+          </tr>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
---
+++ src/assets/color.ts
@@ -0,0 +1,11 @@
+const appColor = {
+    primary: "#001aff80",
+    secundary: "#ffffffCC",
+    ternary: "#00a8ffCC",
+    letter: "#fff",
+    letterDark: "#000",
+    backgroundPrimary: "#003DFF",
+    backgroundSecundary: "#00A8FF",
+}
+
+export default appColor
